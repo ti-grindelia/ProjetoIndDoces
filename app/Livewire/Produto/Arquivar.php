2f84805a36a9dbe5511f9f9a903119de0797e576
ti-grindelia <ti@grindelia.com.br>
2025-10-24 16:08:01 -0300

    Livewire produtos

---
+++ app/Livewire/Produto/Arquivar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Produto;
+
+use Livewire\Component;
+
+class Arquivar extends Component
+{
+    public function render()
+    {
+        return view('livewire.produto.arquivar');
+    }
+}
---
+++ app/Livewire/Produto/Restaurar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Produto;
+
+use Livewire\Component;
+
+class Restaurar extends Component
+{
+    public function render()
+    {
+        return view('livewire.produto.restaurar');
+    }
+}
