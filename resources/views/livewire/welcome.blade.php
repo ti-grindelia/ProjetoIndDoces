153993be9e6a6d48affc194fd1faf9123d54e179
ti-grindelia <ti@grindelia.com.br>
2025-11-11 16:01:51 -0300

    Customizando home

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,9 @@
-<div class="p-2 h-dvh flex items-center justify-center text-xl font-semibold">
-  Olá, vamos começar pelos cadastros?
-</div>
+<x-card class="text-center py-6 mt-8">
+    <h2 class="text-2xl font-semibold mb-2">
+        Bem-vindo ao Sistema de Gerenciamento das Indústrias
+    </h2>
+
+    <p class="text-gray-600">
+        Gerencie cadastros de <strong>usuários</strong>,  <strong>empresas</strong>, <strong>matérias-primas</strong> e <strong>produtos</strong>.
+    </p>
+</x-card>
